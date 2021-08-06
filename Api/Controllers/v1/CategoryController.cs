f78c3e12cd776421e7ea12a784f13c79519b6ecf
sajad kardel <sajadkardel@Gmail.com>
2021-08-07 01:48:28 +0430

    Add Category And Product CRUD Controllers

---
+++ Api/Controllers/v1/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models.Product;
+using AutoMapper;
+using Data.Contracts;
+using Entities.Product;
+using WebFramework.Api;
+
+namespace Api.Controllers.v1
+{
+    public class CategoryController : CrudController<CategoryDto, CategorySelectDto, Category, int>
+    {
+        public CategoryController(IRepository<Category> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
---
+++ Api/Models/Product/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebFramework.Api;
+
+namespace Api.Models.Product
+{
+    public class CategoryDto : BaseDto<CategoryDto, Entities.Product.Category, int>
+    {
+        public string Name { get; set; }
+    }
+}
